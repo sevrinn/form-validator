bd29e9c52e3ce13bae1db5b6722dff9eae1bafe5
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-20 13:08:35 -0700

    added CounterTwo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import CounterOne from './components/CounterOne';
-
+import CounterTwo from './components/CounterTwo';
 function App() {
   return (
     <div className="App">
       <CounterOne />
+      <CounterTwo />
     </div>
   );
 }
---
+++ src/components/CounterTwo.js
@@ -0,0 +1,12 @@
+import React, {useReducer} from 'react';
+
+const CounterTwo = () => {
+
+  return (
+    <div>
+      <h2>Counter Two</h2>
+    </div>
+  );
+};
+
+export default CounterTwo;
